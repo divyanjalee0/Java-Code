f0710685b33aef48e60d8d0660007247b370676b
Divyanjalee Srivastav <srivastavdivyanjalee4@gmail.com>
2026-02-01 02:11:20 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,26 @@
+public class variable {
+    public static void main(){
+        //primitive data type
+        int age = 20;
+
+        double GPA = 9.3;
+
+        char remark = 'A';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+
+        if(isStudent){
+            System.out.println("You are a Student");
+        }
+        else
+            System.out.println("You are NOT a Student");
+        System.out.println("My Age is "  + age);
+        System.out.println("My 3rd sem GPA is " + GPA);
+        System.out.println("My remark for 3rd sem is "+remark);
+
+        String name ="Divyanjalee Srivastav";
+        System.out.println("Hello my name is "+name+" My age is "+age+" My GPA is "+ GPA);
+
+    }
+}
