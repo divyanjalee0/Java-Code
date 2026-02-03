08d346dbb071002e9bdc4e475dc028e30be5502c
Divyanjalee Srivastav <srivastavdivyanjalee4@gmail.com>
2026-02-03 07:10:35 +0530

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,23 @@
+public class Maths{
+     public static void main(String[] args){
+          //System.out.println(Math.PI);
+         //System.out.println(Math.E);
+
+         double result;
+
+         result = Math.pow(2,5);
+         result = Math.abs(-2);
+         result = Math.sqrt(4);
+         result = Math.ceil(2.14);
+         result = Math.floor(5.99);
+         result = Math.max(1,10);
+         result = Math.min(2,3);
+         System.out.println(result);
+
+
+
+     }
+
+
+
+}
